7a477d9971f348e5404ec4f7c06f831850b164be
SAURABH KUMAR TIWARI <sky.royaltiwari@gmail.com>
2022-08-11 12:28:26 +0530

    Constructor created

---
+++ src/FurnitureMart.java
@@ -0,0 +1,21 @@
+
+
+public class FurnitureMart {
+
+   // Attribute
+    int furnitureCode;
+    String furnitureType;
+    char gradeOfFurniture;
+    String furnitureColour;
+    String furnitureUsage;
+    long price;
+    FurnitureMart(){
+        furnitureCode=0;
+        furnitureType="";
+        gradeOfFurniture='\u0000';
+        furnitureColour="";
+        furnitureUsage="";
+
+    }
+
+}
